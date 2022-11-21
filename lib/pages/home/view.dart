a8e8802e05335d4866a73415b70daf892eb326ec
Wing CHAN <wingch.apps@gmail.com>
2022-11-21 22:31:27 +0800

    [feat] add SelectionArea

--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -37,116 +37,107 @@ class _HomePageState extends State<_HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.onPrimary,
-      appBar: AppBar(
-        title: const TextRenderer(
-          style: TextRendererStyle.header1,
-          child: Text('Qr Code Generator'),
+    return SelectionArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.onPrimary,
+        appBar: AppBar(
+          title: const TextRenderer(
+            style: TextRendererStyle.header1,
+            child: Text('Qr Code Generator'),
+          ),
         ),
-      ),
-      body: BlocListener<HomeBloc, HomeState>(
-        listenWhen: (previous, current) =>
-            previous.qrCodeData != current.qrCodeData,
-        listener: (context, state) {
-          if (state.qrCodeData != _qrCodeDataController.text) {
-            _qrCodeDataController.text = state.qrCodeData;
-          }
-        },
-        child: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.8,
-            heightFactor: 1,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                TextRenderer(
-                  text: 'Qr Code image',
-                  style: TextRendererStyle.header2,
-                  child: BlocSelector<HomeBloc, HomeState, String>(
-                    selector: (state) {
-                      return state.qrCodeData;
+        body: BlocListener<HomeBloc, HomeState>(
+          listenWhen: (previous, current) => previous.qrCodeData != current.qrCodeData,
+          listener: (context, state) {
+            if (state.qrCodeData != _qrCodeDataController.text) {
+              _qrCodeDataController.text = state.qrCodeData;
+            }
+          },
+          child: Center(
+            child: FractionallySizedBox(
+              widthFactor: 0.8,
+              heightFactor: 1,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  TextRenderer(
+                    text: 'Qr Code image',
+                    style: TextRendererStyle.header2,
+                    child: BlocSelector<HomeBloc, HomeState, String>(
+                      selector: (state) {
+                        return state.qrCodeData;
+                      },
+                      builder: (context, qrCodeData) {
+                        return QrImage(
+                          data: qrCodeData,
+                          version: QrVersions.auto,
+                          size: 200.0,
+                          dataModuleStyle: QrDataModuleStyle(
+                            color: Theme.of(context).colorScheme.onPrimaryContainer,
+                            dataModuleShape: QrDataModuleShape.square,
+                          ),
+                          eyeStyle: QrEyeStyle(
+                            color: Theme.of(context).colorScheme.onPrimaryContainer,
+                            eyeShape: QrEyeShape.square,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  TextField(
+                    controller: _qrCodeDataController,
+                    focusNode: _qrCodeDataFocusNode,
+                    decoration: const InputDecoration(
+                      labelText: 'Enter a string',
+                    ),
+                    onChanged: (value) {
+                      context.read<HomeBloc>().add(HomeQrCodeDataEntered(value));
                     },
-                    builder: (context, qrCodeData) {
-                      return QrImage(
-                        data: qrCodeData,
-                        version: QrVersions.auto,
-                        size: 200.0,
-                        dataModuleStyle: QrDataModuleStyle(
-                          color:
-                              Theme.of(context).colorScheme.onPrimaryContainer,
-                          dataModuleShape: QrDataModuleShape.square,
-                        ),
-                        eyeStyle: QrEyeStyle(
-                          color:
-                              Theme.of(context).colorScheme.onPrimaryContainer,
-                          eyeShape: QrEyeShape.square,
-                        ),
-                      );
+                    onSubmitted: (value) {
+                      context.read<HomeBloc>().add(HomeQrCodeDataRecordAdded(value));
+                      _qrCodeDataFocusNode.requestFocus();
                     },
                   ),
-                ),
-                TextField(
-                  controller: _qrCodeDataController,
-                  focusNode: _qrCodeDataFocusNode,
-                  decoration: const InputDecoration(
-                    labelText: 'Enter a string',
+                  const SizedBox(height: 16),
+                  TextRenderer(
+                    text: 'Qr Code History',
+                    style: TextRendererStyle.header3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text('History', style: Theme.of(context).textTheme.headline6),
+                        Text('save in local storage', style: Theme.of(context).textTheme.subtitle1),
+                        const SizedBox(height: 8),
+                        BlocSelector<HomeBloc, HomeState, List<String>>(
+                          selector: (state) {
+                            return state.qrCodeHistories;
+                          },
+                          builder: (context, qrCodeHistories) {
+                            return Wrap(
+                              crossAxisAlignment: WrapCrossAlignment.start,
+                              runSpacing: 8,
+                              spacing: 8,
+                              children: [
+                                ...qrCodeHistories.map((qrCodeData) {
+                                  return InputChip(
+                                    label: Text(qrCodeData),
+                                    onPressed: () {
+                                      context.read<HomeBloc>().add(HomeQrCodeDataRecordSelected(qrCodeData));
+                                    },
+                                    onDeleted: () {
+                                      context.read<HomeBloc>().add(HomeQrCodeDataRecordRemoved(qrCodeData));
+                                    },
+                                  );
+                                }),
+                              ],
+                            );
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  onChanged: (value) {
-                    context.read<HomeBloc>().add(HomeQrCodeDataEntered(value));
-                  },
-                  onSubmitted: (value) {
-                    context
-                        .read<HomeBloc>()
-                        .add(HomeQrCodeDataRecordAdded(value));
-                    _qrCodeDataFocusNode.requestFocus();
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextRenderer(
-                  text: 'Qr Code History',
-                  style: TextRendererStyle.header3,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text('History',
-                          style: Theme.of(context).textTheme.headline6),
-                      Text('save in local storage',
-                          style: Theme.of(context).textTheme.subtitle1),
-                      const SizedBox(height: 8),
-                      BlocSelector<HomeBloc, HomeState, List<String>>(
-                        selector: (state) {
-                          return state.qrCodeHistories;
-                        },
-                        builder: (context, qrCodeHistories) {
-                          return Wrap(
-                            crossAxisAlignment: WrapCrossAlignment.start,
-                            runSpacing: 8,
-                            spacing: 8,
-                            children: [
-                              ...qrCodeHistories.map((qrCodeData) {
-                                return InputChip(
-                                  label: Text(qrCodeData),
-                                  onPressed: () {
-                                    context.read<HomeBloc>().add(
-                                        HomeQrCodeDataRecordSelected(
-                                            qrCodeData));
-                                  },
-                                  onDeleted: () {
-                                    context.read<HomeBloc>().add(
-                                        HomeQrCodeDataRecordRemoved(
-                                            qrCodeData));
-                                  },
-                                );
-                              }),
-                            ],
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
